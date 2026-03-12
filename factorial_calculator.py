e73bf2deeee3bad0cf96253a6282bb86e6b0f2d6
michaeli105984-ship-it <michaelii105984@mcpsva.org>
2026-03-12 10:01:57 -0400

    Implement factorial calculation logic

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -1,14 +1,29 @@
 """
 Filename: factorial_calculator.py
-Author: <NAME>
-Created: <DATE>
+Author: <Fontaine, Michael>
+Created: <3/4/2026>
 Instructor: Holtslander
 """
 
 def factorial():
     # Write your code here
-
-
+    print("Enter a non-negative whole number on the line below.")
+    number = input()            
+    number = int(number)          
+    if number == 0:
+        print("0! = 1")
+    else:
+        total = 1
+        answer_string = ""
+        i = number
+        while i >= 1:
+            total = total * i
+            if i == 1:
+                answer_string = answer_string + str(i)
+            else:
+                answer_string = answer_string + str(i) + " * "
+            i = i - 1
+        print(str(number) + "! = " + answer_string + " = " + str(total))
 # You should not need to change any code below this point
 def main():
     print("This program computes factorials and displays their intermediate calculations.")
@@ -19,4 +34,4 @@ def main():
     print("Thank you for using this factorial calculator!")
 
 if __name__ == "__main__":
-    main()
+    main()"""
